fc14f0c7c63da921c148ccd91a94500eebab39d3
sandiarbaa <sandiarbaa@gmail.com>
2024-03-25 07:38:28 +0700

    add: setup tailwind & redesign ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handlePlus = () => {
+    setCount(count + 1);
+  };
+
+  const handleMinus = () => {
+    setCount(count - 1);
+  };
+
+  if (count === 5) {
+    setCount(0);
+  } else if (count === -5) {
+    setCount(0);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-full min-h-screen bg-slate-100 flex items-center justify-center gap-x-2">
+      <Button onClick={handleMinus}>-</Button>
+      <h1 className="text-2xl font-semibold">count is : {count}</h1>
+      <Button onClick={handlePlus}>+</Button>
+    </div>
+  );
 }
 
-export default App
+function Button({ children, onClick }) {
+  return (
+    <button
+      className="bg-black px-4 py-1 rounded-md shadow text-white font-bold text-2xl"
+      // onClick={() => setCount(count - 1)}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
